f2c3cb88b082414ae1c264b71ce0982e6dcad5fe
Christopher <christopher@Christophers-MacBook-Air.local>
2026-06-25 14:34:02 +0800

    phone icon: dropdown of enriched numbers, click-to-call (dial endpoint left open)

--- assets/IconLetterS-DLKY66_n.js
+++
@@ -1,6 +0,0 @@
-import{c as e}from"./index-BMVl-Mtt.js";/**
- * @license @tabler/icons-react v3.34.0 - MIT
- *
- * This source code is licensed under the MIT license.
- * See the LICENSE file in the root directory of this source tree.
- */var a=e("outline","letter-s","IconLetterS",[["path",{d:"M17 8a4 4 0 0 0 -4 -4h-2a4 4 0 0 0 0 8h2a4 4 0 0 1 0 8h-2a4 4 0 0 1 -4 -4",key:"svg-0"}]]);export{a as I};
